026a3faa0db1d2a82d0ea622ca6029068bb3b1e5
otetsconstantine <otetsconstantine@icloud.com>
2025-08-21 19:11:25 +0300

    Add files via upload

--- hydra/js/script.js
+++ hydra/js/script.js
@@ -1,22 +1,24 @@
 "use strict"
 
-window.addEventListener('load', windowLoad)
+// window.addEventListener('load', windowLoad)
 
-let isMobile
+// let isMobile
 
-function windowLoad() {
-	isMobile = window.navigator.userAgentData.mobile
-	isMobile ? document.body.setAttribute('data-touch', '') : null
+// function windowLoad() {
+	// isMobile = window.navigator.userAgentData.mobile
+	// isMobile ? document.body.setAttribute('data-touch', '') : null
 
+	// document.addEventListener('click', documentActions)
+
+	// const coundown = document.querySelectorAll('[data-coundown]')
+	// coundown.length ? initCoundown(coundown) : null
+
+	// dynamicAdaptHeader();
+	// slidersInit();
+// }
 	document.addEventListener('click', documentActions)
-
-	const coundown = document.querySelectorAll('[data-coundown]')
-	coundown.length ? initCoundown(coundown) : null
-
 	dynamicAdaptHeader();
 	slidersInit();
-}
-
 function documentActions(e) {
 	const targetElement = e.target
 	if (targetElement.closest('.icon-menu')) {
@@ -49,30 +51,91 @@ function dynamicAdaptHeader() {
 	}
 }
 
+// function slidersInit() {
+// 	if (document.querySelector('.slider-contacts')) {
+// 		const sliderContacts = new Swiper('.slider-contacts', {
+// 			loop: true,
+// 			// Navigation arrows
+// 				navigation: {
+// 				nextEl: '.contacts__next',
+// 				prevEl: '.contacts__prev',
+// 			},
+// 			breakpoints: {
+// 				320: {
+// 					slidesPerView: 1,
+// 					spaceBetween: 10,
+// 				},
+// 				768: {
+// 					slidesPerView: 2,
+// 					spaceBetween: 20,
+// 				},
+// 				1050: {
+// 					slidesPerView: 3,
+// 					spaceBetween: 25,
+// 				},
+// 			},
+// 		});
+// 	}
+// 	if (document.querySelector('.slider-services')) {
+// 		const sliderServices = new Swiper('.slider-services', {
+// 			loop: true,
+// 			// Navigation arrows
+// 				navigation: {
+// 				nextEl: '.services__next',
+// 				prevEl: '.services__prev',
+// 			},
+// 			breakpoints: {
+// 				320: {
+// 					slidesPerView: 1,
+// 					spaceBetween: 10,
+// 				},
+// 				450: {
+// 					slidesPerView: 2,
+// 					spaceBetween: 10,
+// 				},
+// 				767: {
+// 					slidesPerView: 3,
+// 					spaceBetween: 10,
+// 				},
+// 				1050: {
+// 					slidesPerView: 4,
+// 					spaceBetween: 10,
+// 				},
+// 			},
+// 		});
+// 	}
+// }
 function slidersInit() {
-	if (document.querySelector('.slider-contacts')) {
-		const sliderContacts = new Swiper('.slider-contacts', {
-			loop: true,
-			// Navigation arrows
-				navigation: {
-				nextEl: '.contacts__next',
-				prevEl: '.contacts__prev',
-			},
-			breakpoints: {
-				320: {
-					slidesPerView: 1,
-					spaceBetween: 10,
-				},
-				768: {
-					slidesPerView: 2,
-					spaceBetween: 20,
-				},
-				1050: {
-					slidesPerView: 3,
-					spaceBetween: 25,
-				},
-			},
-			
-		});
-	}
+   const contactsSlider = document.querySelector('.slider-contacts');
+   if (contactsSlider) {
+      new Swiper(contactsSlider, {
+      loop: true,
+      navigation: {
+      nextEl: '.contacts__next',
+		prevEl: '.contacts__prev',
+      },
+      breakpoints: {
+      320: { slidesPerView: 1, spaceBetween: 10 },
+      768: { slidesPerView: 2, spaceBetween: 20 },
+      1050: { slidesPerView: 3, spaceBetween: 25 },
+      },
+   });
+}
+
+	const servicesSlider = document.querySelector('.slider-services');
+	if (servicesSlider) {
+   new Swiper(servicesSlider, {
+      loop: true,
+      navigation: {
+      nextEl: '.services__next',
+      prevEl: '.services__prev',
+      },
+      breakpoints: {
+      320: { slidesPerView: 1, spaceBetween: 10 },
+      500: { slidesPerView: 2, spaceBetween: 10 },
+      767: { slidesPerView: 3, spaceBetween: 10 },
+      1050: { slidesPerView: 4, spaceBetween: 10 },
+      },
+   });
+}
 }
