0d8a70b0e138282c8b096ebe5c5f6dcff6fe11c1
otetsconstantine <otetsconstantine@icloud.com>
2025-08-30 18:11:05 +0300

    Add files via upload

--- hydra/js/script.js
+++ hydra/js/script.js
@@ -5,31 +5,70 @@ window.addEventListener('load', windowLoad)
 let isMobile
 
 function windowLoad() {
-	isMobile = window.navigator.userAgentData.mobile
-	isMobile ? document.body.setAttribute('data-touch', '') : null
+   isMobile = window.navigator.userAgentData 
+      ? window.navigator.userAgentData.mobile 
+      : /Mobi/i.test(navigator.userAgent);
 
-	document.addEventListener('click', documentActions)
+   if (isMobile) document.body.setAttribute('data-touch', '')
 
-	const coundown = document.querySelectorAll('[data-coundown]')
-	coundown.length ? initCoundown(coundown) : null
+   document.addEventListener('click', documentActions)
 
-	dynamicAdaptHeader();
-	slidersInit();
+   const coundown = document.querySelectorAll('[data-coundown]')
+   if (coundown.length) initCoundown(coundown)
+
+   dynamicAdaptHeader();
+   slidersInit();
 }
+
 function documentActions(e) {
-	const targetElement = e.target
-	if (targetElement.closest('.icon-menu')) {
-		document.body.classList.toggle('scroll-lock')
-		document.documentElement.classList.toggle('open-menu')
-	}
+   const targetElement = e.target
+   if (targetElement.closest('.icon-menu')) {
+      document.body.classList.toggle('scroll-lock')
+      document.documentElement.classList.toggle('open-menu')
+   }
 }
+
 const btn = document.querySelector(".technologies__button");
 const block = document.querySelector(".technologies__body");
 
-btn.addEventListener("click", () => {
-	block.classList.toggle("active");
-   btn.classList.toggle("active");
-});
+if (btn && block) {
+   btn.addEventListener("click", () => {
+      block.classList.toggle("active");
+      btn.classList.toggle("active");
+   });
+}
+// "use strict"
+
+// window.addEventListener('load', windowLoad)
+
+// let isMobile
+
+// function windowLoad() {
+// 	isMobile = window.navigator.userAgentData.mobile
+// 	isMobile ? document.body.setAttribute('data-touch', '') : null
+
+// 	document.addEventListener('click', documentActions)
+
+// 	const coundown = document.querySelectorAll('[data-coundown]')
+// 	coundown.length ? initCoundown(coundown) : null
+
+// 	dynamicAdaptHeader();
+// 	slidersInit();
+// }
+// function documentActions(e) {
+// 	const targetElement = e.target
+// 	if (targetElement.closest('.icon-menu')) {
+// 		document.body.classList.toggle('scroll-lock')
+// 		document.documentElement.classList.toggle('open-menu')
+// 	}
+// }
+// const btn = document.querySelector(".technologies__button");
+// const block = document.querySelector(".technologies__body");
+
+// btn.addEventListener("click", () => {
+// 	block.classList.toggle("active");
+//    btn.classList.toggle("active");
+// });
 function dynamicAdaptHeader() {
 	const headerMenu = document.querySelector('.menu-header__body')
 
@@ -104,9 +143,6 @@ function slidersInit() {
    });
 
 }
-
-
-
 	const howSlider = document.querySelector('.how__slider');
 	if (howSlider) {
    new Swiper(howSlider, {
